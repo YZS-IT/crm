74603b25e16e58908b40ea13796fb5013de3bfb3
YZS-IT <511379504@qq.com>
2021-07-11 20:20:13 +0800

    1

---
+++ src/main/java/com/yzs/pojo/User.java
@@ -0,0 +1,55 @@
+package com.yzs.pojo;
+
+import java.util.Objects;
+
+public class User {
+    private Integer id;
+    private int ident;
+    private String name;
+    private String password;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public int getIdent() {
+        return ident;
+    }
+
+    public void setIdent(int ident) {
+        this.ident = ident;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return ident == user.ident && Objects.equals(id, user.id) && Objects.equals(name, user.name) && Objects.equals(password, user.password);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, ident, name, password);
+    }
+}
