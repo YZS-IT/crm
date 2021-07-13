a9a47148165fc6fbd3ac2df242865a224639c955
YZS-IT <511379504@qq.com>
2021-07-13 19:00:11 +0800

    1

--- src/main/java/com/yzs/crm/settings/pojo/User.java
+++ src/main/java/com/yzs/crm/settings/pojo/User.java
@@ -4,19 +4,24 @@ import java.util.Objects;
 
 public class User {
 
-    private String id;
-    private String loginAct;
-    private String name;
-    private String loginPwd;
-    private String email;
-    private String expireTime;
-    private String lockState;
-    private String deptno;
-    private String allowIps;
-    private String createTime;
-    private String createBy;
-    private String editTime;
-    private String editBy;
+    private String id; //主键
+    private String loginAct; //登录账号
+    private String name; //用户真实姓名
+    private String loginPwd; //登录密码
+    private String email; //用户邮箱
+    private String expireTime; //失效时间 yyyy-MM-dd HH:mm:ss 19位
+    private String lockState; //账号状态 ,0锁定,1启用
+    private String deptno; //部门编号
+    private String allowIps; //允许访问的IP地址
+    private String createTime; //创建时间
+    private String createBy; //创建人
+    private String editTime; //修改时间
+    private String editBy; //修改人
+
+    /*
+        关于登录:
+        验证账号密码后,继续验证 expireTime lockState allowIps
+    */
 
     public String getId() {
         return id;
